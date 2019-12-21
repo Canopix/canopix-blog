0f8ab63bd35b35e31359d3999353a213b4aaf4a1
Emanuel Canova <emanuel.canova@mooveit.com>
2019-12-21 13:09:57 -0300

    ADD Markdown post

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,9 +27,14 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/blog`,
+      },
+    },
+    `gatsby-transformer-remark`,
   ],
 }
