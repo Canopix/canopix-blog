9f8bad8b9684ef3a319ad19ee13a26ef897ca6f0
EmanuelCanova <canova.9400@gmail.com>
2020-04-04 03:20:20 -0300

    Add more style and images

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,6 +13,8 @@ import Header from "./header"
 import Sidebar from './Sidebar'
 import "./layout.css"
 import wave from './../static/svg/wave.svg'
+import gatsbyLogo from './../static/svg/gatsby-monogram.svg'
+import netlify from './../static/svg/netlify.svg'
 
 
 const Layout = ({ children }) => {
@@ -56,10 +58,18 @@ const Layout = ({ children }) => {
             <main className="flex-grow">{children}</main>
             <Sidebar categories={data.allMdx.distinct} />
           </div>
-          <footer className="bottom-0 Nunito-800 text-yellow-500">
-            © {new Date().getFullYear()},
-          {` `}
-            <a href="https://www.emanuelcanova.com">Emanuel Canova</a>
+          <footer className="bottom-0 mt-8 Nunito-200 text-sm flex flex-col">
+            <div className="text-gray-200">
+              © {new Date().getFullYear()},
+              {` `}
+              <a  href="https://www.emanuelcanova.com">Emanuel Canova</a>
+            </div>
+            <div className="flex flex-row">
+              <p>Made with </p>
+              <a href="https://www.gatsbyjs.org/" target="_blank"><img style={{width: 25, height: 25, margin: "0 10px 0 10px" }} src={gatsbyLogo} /></a>
+              <p> deployed by </p>
+              <a href="https://www.netlify.com/" target="_blank"><img style={{width: 25, height: 25, margin: "0 10px 0 10px" }} src={netlify} /></a>
+            </div>
           </footer>
         </div>
       </div>
