6315a97fb3e1d2ac7d395adbf0ae02d394983f88
Emanuel Canova <emanuel.canova@mooveit.com>
2020-06-22 22:37:36 -0300

    Add Talk about Variable Font

--- src/components/Talk.js
+++ src/components/Talk.js
@@ -2,28 +2,26 @@ import React from "react"
 
 const Talk = ({ talk }) => {
   return (
-    <div className="my-8  p-4 flex flex-col md:flex-row md:items-center ">
-      <div className="w-full md:w-32">
-        <h1 className="Nunito-800 text-yellow-500 m-0 text-2xl">
-          {talk.title}
-        </h1>
-      </div>
-      <div className="md:p-4 my-2 md:mx-0 w-full md:w-64">
-        <p className="m-0 Nunito-200 text-sm">Language: {talk.language}</p>
-        <p className="m-0 Nunito-200 text-sm">Date: {talk.date}</p>
-        <p className="m-0 Nunito-200 text-sm">
-          Organization:
-          <a
-            href={talk.organizationLink}
-            target="_blank"
-            className="underline Nunito-800 text-yellow-500"
-          >
-            {talk.organization}
-          </a>
-        </p>
-      </div>
-      <div>
-        <p className="mt-2 Nunito-400 italic text-lg">{talk.description}</p>
+    <div className="my-4">
+      <h1 className="Nunito-800 text-yellow-500 m-0 text-2xl">{talk.title}</h1>
+      <div className="flex flex-col md:flex-row md:items-center ">
+        <div className="md:p-4 my-2 md:mx-0 w-full md:w-1/2">
+          <p className="m-0 Nunito-200 text-sm">Language: {talk.language}</p>
+          <p className="m-0 Nunito-200 text-sm">Date: {talk.date}</p>
+          <p className="m-0 Nunito-200 text-sm">
+            Organization:
+            <a
+              href={talk.organizationLink}
+              target="_blank"
+              className="underline Nunito-800 text-yellow-500"
+            >
+              {talk.organization}
+            </a>
+          </p>
+        </div>
+        <div className="md:w-1/2">
+          <p className="mt-2 Nunito-400 italic text-lg ">{talk.description}</p>
+        </div>
       </div>
     </div>
   )
--- src/data/talks.js
+++ src/data/talks.js
@@ -1,24 +1,41 @@
 export default [
-    {
-        title: 'Suspense: What?! Why?!',
-        image: 'SuspenseReactTalk',
-        language: 'Spanish',
-        description: 'What is Suspense? Why everyone is expecting this feature for Data Fetching. This talk is about this!',
-        organization: 'ReactCba (React Cordoba)',
-        organizationLink: 'https://linktr.ee/reactcba.ok',
-        date: '01/30/2020',
-        link: 'https://www.instagram.com/p/B7ZNlTvFOy2/',
-        slides: 'https://drive.google.com/open?id=1tk8A4PVJ4BhiqUXSXfh40fiutiILuOma'
-    },
-    {
-        title: 'Micro Frontends, What?! Why?!',
-        image: 'microfrontendsTalk',
-        language: 'Spanish',
-        description: 'A little overview of What is Micro Frontend and how you can implement it on your project.',
-        organization: 'BeerJs Cba',
-        organizationLink: 'https://www.beerjscba.com',
-        date: '07/25/2019',
-        link: 'https://www.youtube.com/watch?v=0YNDKEP5_Dk',
-        slides: 'https://drive.google.com/open?id=15Y62xvnKoeVVRKdTKwA8PmW3T6bXm8UukvI9ZUC5bmI'
-    }
+  {
+    title: "Un elegante camino hacia las Variable Fonts",
+    image: "VariableFontsTalk",
+    language: "Spanish",
+    description:
+      "What are Variable Fonts? This talk is intended to dive into the fantastic features of Variables Fonts, What are? Why do we need to use it?  This talk pretends to motivate the curiosity of the audience into this topic",
+    organization: "CodeAr (Web Conf 2020 Argentina Round of Talks)",
+    organizationLink: "https://codear.org/",
+    date: "06/22/2020",
+    link: "https://youtu.be/mT1hLoFzCVc?t=2952",
+    slides:
+      "https://drive.google.com/file/d/131njKljTpWKSmlhfGRvD_Gqjar8__Ras/view?usp=sharing",
+  },
+  {
+    title: "Suspense: What?! Why?!",
+    image: "SuspenseReactTalk",
+    language: "Spanish",
+    description:
+      "What is Suspense? Why everyone is expecting this feature for Data Fetching. This talk is about this!",
+    organization: "ReactCba (React Cordoba)",
+    organizationLink: "https://linktr.ee/reactcba.ok",
+    date: "01/30/2020",
+    link: "https://www.instagram.com/p/B7ZNlTvFOy2/",
+    slides:
+      "https://drive.google.com/file/d/1tk8A4PVJ4BhiqUXSXfh40fiutiILuOma/view?usp=sharing",
+  },
+  {
+    title: "Micro Frontends, What?! Why?!",
+    image: "microfrontendsTalk",
+    language: "Spanish",
+    description:
+      "A little overview of What is Micro Frontend and how you can implement it on your project.",
+    organization: "BeerJs Cba",
+    organizationLink: "https://www.beerjscba.com",
+    date: "07/25/2019",
+    link: "https://www.youtube.com/watch?v=0YNDKEP5_Dk",
+    slides:
+      "https://drive.google.com/open?id=15Y62xvnKoeVVRKdTKwA8PmW3T6bXm8UukvI9ZUC5bmI",
+  },
 ]
